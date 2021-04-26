704cf16b68d2da8fe1acf19a53dff0ab822994c8
Jacques <daxxas158@gmail.com>
2021-04-26 21:18:47 +0200

    fixed double generation on first world

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -18,18 +18,6 @@ public class LevelManager : MonoBehaviour
         _instance = this;
     }
 
-    private void Start()
-    {
-        Debug.Log("Start method level manager");
-        var levelGenerator = FindObjectOfType<LevelGenerator>();
-        if (levelGenerator != null)
-        {
-            Debug.Log("generate level called");
-            levelGenerator.GenerateLevel(levelBiomeOrder[currentBiomeIndex], levelBiomeSize[currentBiomeIndex]);
-        }
-    }
-
-    
     [ContextMenu("Switch to next level")]
     public void SwitchToNextLevel()
     {
